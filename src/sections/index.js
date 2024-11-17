16c72a81b8682382c25c207274766e1c74a57ffb
FlyFu-0 <tihonov-04@yandex.ru>
2024-11-17 13:17:18 +0300

    base setup, assets. Navbar

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    SpecialOffer,
+    SuperQuality,
+    Services,
+    Subscribe,
+};
