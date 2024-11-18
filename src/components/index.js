c59542de6fe9ab46903b42c6423ec9bd3b80cbe7
FlyFu-0 <tihonov-04@yandex.ru>
2024-11-18 15:41:24 +0300

    service section

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,13 @@ import PopularProductCard from "./PopularProductCard";
 import Nav from "./Nav";
 import Button from "./Button";
 import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
+
 
 export {
     PopularProductCard,
     Nav,
     Button,
-    ShoeCard
+    ShoeCard,
+    ServiceCard
 }
