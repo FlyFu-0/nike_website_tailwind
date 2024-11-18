c7ef5c6026d3be2a839090a17415b33abae703b8
FlyFu-0 <tihonov-04@yandex.ru>
2024-11-18 15:14:17 +0300

    Popular products card

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import PopularProductCard from "./PopularProductCard";
+import Nav from "./Nav";
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+
+export {
+    PopularProductCard,
+    Nav,
+    Button,
+    ShoeCard
+}
